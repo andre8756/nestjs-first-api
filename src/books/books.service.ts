328c000d4b1cd210f657830062d4e221704dfbc3
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-02-11 20:53:22 -0300

    service completed

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './book.entity';
+import { BookDto } from './dto/book.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+    constructor(
+        @InjectRepository(Book)
+        private readonly bookRepository: Repository<Book>,
+    ){}
+
+    async findAll(): Promise<Book[]>{
+        return this.bookRepository.find();
+    }
+
+    async findOne(id: number): Promise<Book | null> {
+        return this.bookRepository.findOneBy({ id });
+    }
+
+    async create(bookDto: BookDto): Promise<Book> {
+        const book = this.bookRepository.create(bookDto);
+        return this.bookRepository.save(book);
+    }
+
+    async update(id: number, bookDto: BookDto): Promise<Book | null>{
+        const book = await this.findOne(id);
+        if(!book) return null;
+
+        Object.assign(book, bookDto);
+        return this.bookRepository.save(book);
+    }
+
+    async remove(id: number): Promise<void>{
+        await this.bookRepository.delete(id);
+    }
+
+}
