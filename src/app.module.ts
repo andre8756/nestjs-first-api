7b1fbaf09a1e619dfb3ccc0db2003c733a6385fd
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-02-11 20:28:25 -0300

    add service, controller and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres', // coloque sua senha
+      database: 'books_db',
+      autoLoadEntities: true,
+      synchronize: true, // só para desenvolvimento
+    }),
+    BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/dto/create-book.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateBookDto {
-  readonly title: string;
-  readonly author: string;
-  readonly published: number;
-}
